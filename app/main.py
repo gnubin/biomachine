5668c6e00d26d38b994b3d2138e48354ed96b81b
nub <alexsms228@gmail.com>
2026-01-05 03:55:42 +0300

    edit main.py. add default route

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from fastapi.templating import Jinja2Templates
+
+
 from app.api.routes import auth
 
 app = FastAPI(
     title="Biomachine",
-    docs_url=None,
-    redoc_url=None,
+    description="Biomachine is a platform for managing and analyzing biological data.",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
 )
+
+templates = Jinja2Templates(directory="app/templates")
 
 app.mount(
     "/static",
@@ -16,3 +23,11 @@ app.mount(
 )
 
 app.include_router(auth.router)
+
+
+@app.get("/")
+def index_page():
+    return "Hello, World!"
+
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000) 
